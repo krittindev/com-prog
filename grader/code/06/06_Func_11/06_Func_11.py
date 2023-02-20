93b370a472954b16e80c3b38315a85450760c326
Krittin Kunaree <39996277+tuliptgr@users.noreply.github.com>
2023-02-21 02:45:15 +0700

    feat: 06_Func_11

--- grader/code/06/06_Func_11/06_Func_11.py
+++ grader/code/06/06_Func_11/06_Func_11.py
@@ -1,11 +1,2 @@
-def bin(x):
-    # Return a binary string prefixed with "0b" constructed from an integer x.
-    pass
-
-
-def int(x, base):
-    # Return an integer constructed from a string x in the specified base (default is 10).
-    pass
-
-
-a, b = input().split()
+a, b = input().strip().split()
+print(bin(int(a, 2) + int(b, 2))[2:])
