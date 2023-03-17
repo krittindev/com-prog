9828c14e77c8877d8d3c56067bd3da1a26342484
Krittin Kunaree <krittinkunaree@gmail.com>
2023-03-17 10:57:30 +0700

    chore: to git 🐙

---
+++ grader/code/08/08_Dict_11/08_Dict_11.py
@@ -0,0 +1,12 @@
+def reverse(d):
+    # d เป็น dict ที่มี value ไม่ซ ้ํากัน
+    # คืน dict ใหม่ที่เก็บ key,value ที่ค่ําเป็น value,key ของ d ที่ได้รับ
+    pass
+
+
+def keys(d, v):
+    # คืนลิสต์ที่เก็บค่ําของ keys ใน d (เรียงยังไงก็ได้) ที่มีค่ํา value เท่ํากับ v
+    pass
+
+
+exec(input().strip())    # ต้องมีค ําสั่งนี้ ตรงนี้ ตอนส่งให้ Grader ตรวจ
